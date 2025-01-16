3f5fc352a48d2347773c39a6784a7da03de2eb73
Vadim Tsvetkov <vadim.tsvetkov80@gmail.com>
2025-01-16 23:13:32 +0300

    feat(core): Apix.Schema.Errors.VoidAstError should be a warning

--- lib/apix.schema/warnings/void_ast_warning.ex
+++ lib/apix.schema/warnings/void_ast_warning.ex
@@ -1,19 +1,22 @@
-defmodule Apix.Schema.Errors.VoidAstError do
+defmodule Apix.Schema.Warnings.VoidAstWarning do
   alias Apix.Schema.Ast
+  alias Apix.Schema.Ast.Meta
 
-  defexception [:message, :ast]
+  defexception [:message, :ast, :meta]
 
   @type t() :: %__MODULE__{
           __exception__: true,
           message: String.t(),
-          ast: Ast.t()
+          ast: Ast.t(),
+          meta: Meta.t() | nil
         }
 
   @impl true
   def exception(%Ast{} = ast) do
     %__MODULE__{
       message: "#{inspect ast} results in `#{inspect Void}.t()`",
-      ast: ast
+      ast: ast,
+      meta: ast.meta
     }
   end
 end

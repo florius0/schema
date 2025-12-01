adf399517a93e842c95473a16d91bc460019897f
Vadim Tsvetkov <vadim.tsvetkov80@gmail.com>
2025-12-01 05:38:51 +0300

    ci: treat warnings as errors

--- .check/tools/compiler.sh
+++ .check/tools/compiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p reports/compiler
-mix compile.machine --format sarif --output reports/compiler/sarif.json 1> >(tee reports/compiler/stdout.log) 2> >(tee reports/compiler/stderr.log >&2)
+mix compile.machine --warnings-as-errors --format sarif --output reports/compiler/sarif.json 1> >(tee reports/compiler/stdout.log) 2> >(tee reports/compiler/stderr.log >&2)
 EXIT_CODE=$?
 echo $EXIT_CODE >reports/compiler/exit_code
 exit $EXIT_CODE

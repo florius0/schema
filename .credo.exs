55fe8f30e4686906c2e192d6f0666d078afd60b0
Vadim Tsvetkov <vadim.tsvetkov80@gmail.com>
2025-12-03 04:38:00 +0300

    ci: configure credo

--- .credo.exs
+++ .credo.exs
@@ -3,11 +3,14 @@
     %{
       name: "default",
       checks: %{
+        enabled: [
+          {Credo.Check.Refactor.Nesting, max_nesting: 3},
+          {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 15}
+        ],
         disabled: [
           {Credo.Check.Consistency.ExceptionNames, false},
           {Credo.Check.Design.TagTODO, false},
-          {Credo.Check.Readability.WithSingleClause, false},
-          {Credo.Check.Refactor.CyclomaticComplexity, max_complexity: 15}
+          {Credo.Check.Readability.WithSingleClause, false}
         ]
       }
     }

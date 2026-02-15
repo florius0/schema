cbc7320e285da85cb5c41bf3ffce66e51186c5e0
Vadim Tsvetkov <vadim.tsvetkov80@gmail.com>
2026-02-15 19:49:07 +0300

    chore: housekeeping

---
+++ .iex.exs
@@ -0,0 +1,8 @@
+use Apix.Schema
+
+alias Apix.Schema.Ast
+alias Apix.Schema.Context
+alias Apix.Schema.Extensions.Core, as: C
+alias Apix.Schema.Extensions.Core.LocalReference, as: L
+alias Apix.Schema.Extensions.Elixir, as: E
+alias Apix.Schema.Extensions.TypeGraph, as: T
